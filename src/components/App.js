78ef13e1202618fddb6371de2de25af27112e977
Rena <rena.kuai@gmail.com>
2022-10-23 16:15:53 -0500

    Set up global tokens, installed style-dictionary

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import './_app.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
